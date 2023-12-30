48724b6f07d16b7f14c823e30348180041e68538
18Preetis <155206084+18Preetis@users.noreply.github.com>
2023-12-30 05:45:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// (function(){
+
+//   let screen = document.querySelector('.screen');
+//   let buttons = document.querySelector('.btn');
+//   let clear = document.querySelector('.btn-clear');
+//   let  equal= document.querySelector('.btn-equal');
+
+// buttons.forEach(function(button){
+//   button.addEventListner('click',function(e){
+//     let value= e.target.dataset.num;
+//     screen.value +=value;
+//   })
+// });
+
+  
+// })();
+
+
+
+
+
+  
